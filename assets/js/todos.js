46a40f1577cc3d7fb6c4d2c4a68e7c76258cfbf9
Bang Nguyen <bangnv77@gmail.com>
2017-01-11 22:26:54 -0500

    toggle the effects of color and text-decoration for an li when clicked

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+// alert("Connected!");
+// check off specific Todos by clicking
+
+// $("li").click(function() {
+// 	$(this).css("color", "gray");
+// 	$(this).css("text-decoration", "line-through");
+// }); this is them same as belows
+
+$("li").click(function() {
+	// if li is gray
+	if($(this).css("color") === "rgb(128, 128, 128)") {
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+		});
+	} else {
+		// turn it gray
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+	}
+})
