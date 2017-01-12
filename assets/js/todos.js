9122a36ad1fa75f29c197a3e1b308dc3af3b7a7a
Bang Nguyen <bangnv77@gmail.com>
2017-01-11 22:29:08 -0500

    refactor the effect by using toggleClass

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -6,18 +6,22 @@
 // 	$(this).css("text-decoration", "line-through");
 // }); this is them same as belows
 
+// $("li").click(function() {
+// 	// if li is gray
+// 	if($(this).css("color") === "rgb(128, 128, 128)") {
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+// 	} else {
+// 		// turn it gray
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// });
+
 $("li").click(function() {
-	// if li is gray
-	if($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	} else {
-		// turn it gray
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
-})
+	$(this).toggleClass("completed");
+});
